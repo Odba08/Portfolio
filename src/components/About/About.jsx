b64c7e5314d07d8dc127c5abc85635e7efd09382
Oscar Daniel Bueno Alvillar <oscar.bueno@adm.urbe.edu>
2025-10-10 13:38:46 -0400

    Portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,12 +4,12 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
+import laptopImg from "../../Assets/aboutme.png";
 import Toolstack from "./Toolstack";
 
 function About() {
   return (
-    <Container fluid className="about-section">
+    <Container fluid className='about-section'>
       <Particle />
       <Container>
         <Row style={{ justifyContent: "center", padding: "10px" }}>
@@ -22,26 +22,36 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Know Who <strong className="purple">I'M</strong>
+              Know Who <strong className='purple'>I'M</strong>
             </h1>
             <Aboutcard />
           </Col>
           <Col
             md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
+            style={{
+              paddingTop: "120px",
+              paddingBottom: "50px",
+              display: "flex",
+              justifyContent: "center",
+            }}
+            className='about-img'
           >
-            <img src={laptopImg} alt="about" className="img-fluid" />
+            <img
+              src={laptopImg}
+              alt='about'
+              className='img-fluid'
+              style={{ maxHeight: "400px", objectFit: "contain" }}
+            />
           </Col>
         </Row>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
+        <h1 className='project-heading'>
+          Professional <strong className='purple'>Skillset </strong>
         </h1>
 
         <Techstack />
 
-        <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
+        <h1 className='project-heading'>
+          <strong className='purple'>Tools</strong> I use
         </h1>
         <Toolstack />
 
